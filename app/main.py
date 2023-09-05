418d89ec1913c37a7e5fe6397e3c644cc8a85990
axellotl <alex.richter39@gmail.com>
2023-09-05 23:37:45 +0200

    Changes.

--- app/main.py
+++ app/main.py
@@ -8,6 +8,9 @@ app = FastAPI()
 
 
 class Item(BaseModel):
+    """
+        Item Klasse
+    """
     id: int
     name: str
     description: str | None = None
@@ -18,16 +21,32 @@ class Item(BaseModel):
 
 @app.get("/hello")
 def hello_world():
+    """
+    Hello World
+
+    :return:
+    """
     return {"hello": "world"}
 
 
 @app.post("/items/")
 def create_item(item: Item) -> Item:
+    """
+    Create Item Test
+
+    :param item:
+    :return:
+    """
     return item
 
 
 @app.get("/items/")
 def read_items() -> list[Item]:
+    """
+    List Item Test
+
+    :return:
+    """
     return [
         Item(name="Portal Gun", price=42.0, id=1),
         Item(name="Plumbus", price=32.0, id=2),
@@ -36,4 +55,10 @@ def read_items() -> list[Item]:
 
 @app.get("/items/{id}")
 def read_item(item_id: int) -> Item:
+    """
+    List One Item Test
+
+    :param item_id:
+    :return:
+    """
     return Item(name="Portal Gun", price=42.0, id=item_id)

df23d733c81c56d67562a9b98ab0720a8dfb0a78
Alexander Richter <a.richter@geocapture.de>
2023-09-05 09:28:03 +0200

    init.

---
+++ test/test_app.py
@@ -0,0 +1,29 @@
+"""
+Test implementation of a Restful API
+"""
+from flask import Flask
+from flask_restful import Api, Resource
+
+CFG_PORT = 5000
+app = Flask(__name__)
+api = Api(app)
+
+
+class HelloWorld(Resource):
+    """
+    Test API Endpoint
+    """
+    def get(self):
+        """
+        Getter for hello world
+        """
+
+        data={"data": "Hi Programming Project 2023!"}
+
+        return data
+
+
+api.add_resource(HelloWorld, '/hello')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=CFG_PORT)

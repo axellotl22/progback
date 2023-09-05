cb567c8b708e0f890fdec7e225ea4a6b14f5c726
Alexander Richter <a.richter@geocapture.de>
2023-09-05 09:35:52 +0200

    init.

--- test/test_app.py
+++ test/test_app.py
@@ -1,29 +1,21 @@
-"""
-Test implementation of a Restful API
-"""
-from flask import Flask
-from flask_restful import Api, Resource
-
-CFG_PORT = 5000
-app = Flask(__name__)
-api = Api(app)
+from app.main import app, api, HelloWorld, CFG_PORT
 
 
-class HelloWorld(Resource):
+def test_setup():
     """
-    Test API Endpoint
+    Tests the setup of the API endpoint
     """
-    def get(self):
-        """
-        Getter for hello world
-        """
+    assert app is not None
+    assert api is not None
+    assert CFG_PORT == 5000 # required to properly setup preceeding deployment step
 
-        data={"data": "Hi Programming Project 2023!"}
-
-        return data
+    assert HelloWorld is not None
 
 
-api.add_resource(HelloWorld, '/hello')
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=CFG_PORT)
+def test_endpoint():
+    """
+    Tests the endpoint contract
+    """
+    hello_world = HelloWorld()
+    res = hello_world.get()
+    assert res['data'] is not None

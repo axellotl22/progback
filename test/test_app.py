a4e077a36954ab8c3970aba524ebfa8da0fafbd5
Andrej Gossen <a.gossen@bb-verpackungen.de>
2023-09-16 22:25:57 +0200

    Erweiterung und Verbesserung der Tests für die App
    - Neue Tests hinzugefügt, um die veränderte Spalten-Parsing-Logik abzudecken.
    - Fehler in den Tests behoben, die durch Änderungen in der Rückgabe von `perform_clustering` entstanden sind.

--- test/test_app.py
+++ test/test_app.py
@@ -7,7 +7,7 @@ from fastapi.testclient import TestClient
 from app.main import app
 from app.services.clustering_service import (
     load_dataframe, clean_dataframe, determine_optimal_clusters,
-    perform_clustering
+    perform_clustering, select_columns
 )
 
 # Setzen des Test Mode auf True
@@ -23,15 +23,9 @@ class TestApp:
     @classmethod
     def setup_class(cls):
         """ Setup für die gesamte Testklasse. Wird einmalig vor allen Tests ausgeführt. """
-
-        # Überprüfen, ob die Quelldatei existiert
-        assert os.path.exists(
-            TEST_DATA_ORIGINAL_PATH), f"Quelldatei {TEST_DATA_ORIGINAL_PATH} nicht gefunden."
-
-        # Überprüfen und erstellen Sie das Zielverzeichnis, falls es nicht existiert
+        assert os.path.exists(TEST_DATA_ORIGINAL_PATH), f"Quelldatei {TEST_DATA_ORIGINAL_PATH} nicht gefunden."
         if not os.path.exists("temp_files/"):
             os.makedirs("temp_files/")
-
         shutil.copy(TEST_DATA_ORIGINAL_PATH, TEST_DATA_COPY_PATH)
 
     @classmethod
@@ -42,7 +36,6 @@ class TestApp:
 
     def test_setup(self):
         """ Überprüft die Initialisierung des API-Endpunkts und der Dienste. """
-
         assert app is not None
         assert load_dataframe is not None
         assert clean_dataframe is not None
@@ -50,57 +43,79 @@ class TestApp:
         assert perform_clustering is not None
 
     def test_clustering_endpoint_with_specified_clusters(self):
-        """
-        Testet den Clustering-Endpunkt mit einer vom Benutzer angegebenen Clusteranzahl.
-        """
+        """Testet den Clustering-Endpunkt mit einer vom Benutzer angegebenen Clusteranzahl."""
         with open(TEST_DATA_COPY_PATH, "rb") as file:
-            # Festlegen der Clusteranzahl auf 3 für diesen Test
-            response = client.post(
-                "/clustering/perform-kmeans-clustering/", 
-                files={"file": file}, data={"clusters": 3})
-
+            response = client.post("/clustering/perform-kmeans-clustering/", files={"file": file}, data={"clusters": 3})
         assert response.status_code == 200
-
         data = response.json()
         assert "points" in data
         assert "centroids" in data
         assert "point_to_centroid_mappings" in data
-
-        # Überprüfen, ob die Anzahl der Zentroide der angegebenen Clusteranzahl entspricht
+        assert "x_label" in data
+        assert "y_label" in data
         actual_clusters = len(data["centroids"])
         assert actual_clusters >= 2
 
     def test_clustering_endpoint(self):
-        """
-        Testet den Clustering-Endpunkt.
-        Testdatei wird hochgeladen und die Antwort überprüft. 
-        """
+        """Testet den Clustering-Endpunkt."""
         with open(TEST_DATA_COPY_PATH, "rb") as file:
             response = client.post("/clustering/perform-kmeans-clustering/", files={"file": file})
-
         assert response.status_code == 200
-
         data = response.json()
         assert "points" in data
         assert "centroids" in data
         assert "point_to_centroid_mappings" in data
-
+        assert "x_label" in data
+        assert "y_label" in data
         data_frame = pd.read_excel(TEST_DATA_COPY_PATH)
         assert len(data["points"]) == len(data_frame)
 
-    def test_dataframe_functions(self):
-        """ Testet die DataFrame-Hilfsfunktionen mit den Testdaten. """
+    def test_clustering_endpoint_with_columns_as_string(self):
+        """Testet den Clustering-Endpunkt mit columns als String-Form."""
+        with open(TEST_DATA_COPY_PATH, "rb") as file:
+            response = client.post("/clustering/perform-kmeans-clustering/", files={"file": file}, data={"columns": "[0,2]"})
+        assert response.status_code == 200
+        data = response.json()
+        assert "points" in data
+        assert "centroids" in data
+        assert "point_to_centroid_mappings" in data
+        assert "x_label" in data
+        assert "y_label" in data
 
+    def test_clustering_endpoint_with_columns_as_list(self):
+        """Testet den Clustering-Endpunkt mit columns als Listenform."""
+        with open(TEST_DATA_COPY_PATH, "rb") as file:
+            response = client.post("/clustering/perform-kmeans-clustering/", files={"file": file}, data={"columns": "0,2"})
+        assert response.status_code == 200
+        data = response.json()
+        assert "points" in data
+        assert "centroids" in data
+        assert "point_to_centroid_mappings" in data
+        assert "x_label" in data
+        assert "y_label" in data
+
+    def test_dataframe_functions(self):
+        """Testet die DataFrame-Hilfsfunktionen mit den Testdaten."""
         data_frame = load_dataframe(TEST_DATA_COPY_PATH)
         assert isinstance(data_frame, pd.DataFrame)
         assert not data_frame.empty
-
+        
         cleaned_data_frame = clean_dataframe(data_frame)
         assert not cleaned_data_frame.empty
-
+        
         optimal_clusters = determine_optimal_clusters(cleaned_data_frame)
         assert optimal_clusters > 0
-
-        kmeans_obj = perform_clustering(cleaned_data_frame, optimal_clusters)
-        labels = kmeans_obj.labels_
+        
+        kmeans_results = perform_clustering(cleaned_data_frame, optimal_clusters)
+        
+        # Extrahieren der Cluster-Zuordnungen aus den 'points'
+        labels = [point['cluster'] for point in kmeans_results['points']]
         assert len(labels) == len(cleaned_data_frame)
+
+
+    def test_select_columns_with_test_data(self):
+        """Testet die select_columns-Hilfsfunktion mit den Testdaten."""
+        data_frame = load_dataframe(TEST_DATA_COPY_PATH)
+        selected_df = select_columns(data_frame, columns=[0, 2])
+        assert list(selected_df.columns) == ["Merkmal 1", "Merkmal 3"]
+        assert selected_df.shape[1] == 2

9b96fc4d3c7df7dfed18518ddc45229eb0bd426d
Andrej Gossen <a.gossen@bb-verpackungen.de>
2023-09-20 23:03:18 +0200

    new tests for deployment

--- test/test_app.py
+++ test/test_app.py
@@ -2,22 +2,19 @@
 
 import os
 import shutil
-import pandas as pd
 from fastapi.testclient import TestClient
 from app.main import app
 
-from app.services.clustering_service import (
-    determine_optimal_clusters, perform_clustering
-)
-from progback.app.services.utils import (
-    load_dataframe, clean_dataframe, select_columns
-)
-
+# Setzen der Umgebungsvariable für den Testmodus
 os.environ["TEST_MODE"] = "True"
 
 client = TestClient(app)
-TEST_DATA_ORIGINAL_PATH = "test/test_daten.xlsx"
-TEST_DATA_COPY_PATH = f"temp_files/{os.path.basename(TEST_DATA_ORIGINAL_PATH)}"
+ENDPOINT_URL = "/clustering/perform-kmeans-clustering/"
+BASE_TEST_DIR = "test/"
+TEMP_FILES_DIR = "temp_files/"
+
+BASIC_TEST_FILE = os.path.join(BASE_TEST_DIR, "kmeans_basic_test.csv")
+ADVANCED_TEST_FILE = os.path.join(BASE_TEST_DIR, "kmeans_advanced_test.csv")
 
 
 class TestApp:
@@ -25,73 +22,44 @@ class TestApp:
 
     @classmethod
     def setup_class(cls):
+        """ Überprüfen, ob die Testdateien vorhanden sind"""
         assert os.path.exists(
-            TEST_DATA_ORIGINAL_PATH), f"Quelldatei {TEST_DATA_ORIGINAL_PATH} nicht gefunden."
-        if not os.path.exists("temp_files/"):
-            os.makedirs("temp_files/")
-        shutil.copy(TEST_DATA_ORIGINAL_PATH, TEST_DATA_COPY_PATH)
+            BASIC_TEST_FILE), f"Datei {BASIC_TEST_FILE} nicht gefunden."
+        assert os.path.exists(
+            ADVANCED_TEST_FILE), f"Datei {ADVANCED_TEST_FILE} nicht gefunden."
+        # Erstellen des temporären Verzeichnisses, wenn es nicht existiert
+        if not os.path.exists(TEMP_FILES_DIR):
+            os.makedirs(TEMP_FILES_DIR)
 
     @classmethod
     def teardown_class(cls):
-        if os.path.exists(TEST_DATA_COPY_PATH):
-            os.remove(TEST_DATA_COPY_PATH)
+        """ Entfernen von temporären Dateien nach Abschluss der Tests"""
+        shutil.rmtree(TEMP_FILES_DIR, ignore_errors=True)
 
-    def test_setup(self):
-        assert app is not None
-        assert load_dataframe is not None
-        assert clean_dataframe is not None
-        assert determine_optimal_clusters is not None
-        assert perform_clustering is not None
-
-    def test_clustering_endpoint_with_specified_clusters(self):
-        with open(TEST_DATA_COPY_PATH, "rb") as file:
+    def test_basic(self):
+        """Teste den Endpoint mit allen Parametern."""
+        with open(BASIC_TEST_FILE, "rb") as f:
             response = client.post(
-                "/clustering/perform-kmeans-clustering/",
-                files={"file": file}, data={"clusters": 3}
+                ENDPOINT_URL,
+                files={"file": f}
             )
         assert response.status_code == 200
-        data = response.json()
-        assert "cluster" in data
-        assert "name" in data
+        assert "name" in response.json()
+        assert "cluster" in response.json()
 
-    def test_clustering_endpoint(self):
-        with open(TEST_DATA_COPY_PATH, "rb") as file:
-            response = client.post(
-                "/clustering/perform-kmeans-clustering/", files={"file": file})
-        assert response.status_code == 200
-        data = response.json()
-        assert "cluster" in data
-        assert "name" in data
+    def test_advanced(self):
+        """Teste den Endpoint mit verschiedenen Kombinationen von Parametern."""
+        with open(ADVANCED_TEST_FILE, "rb") as f:
+            # Ohne Clusterangabe aber mit Distanzangabe
+            response_1 = client.post(
+                ENDPOINT_URL,
+                files={"file": f},
+                data={"column1": 4, 
+                      "column2": 5, 
+                      "distanceMetric": "JACCARDS"}
+            )
+            assert response_1.status_code == 200
+            assert "name" in response_1.json()
+            assert "cluster" in response_1.json()
 
-    def test_clustering_endpoint_with_column_inputs(self):
-        with open(TEST_DATA_COPY_PATH, "rb") as file:
-            response = client.post("/clustering/perform-kmeans-clustering/",
-                                   files={"file": file}, data={"columns": "[0,2]"})
-        assert response.status_code == 200
-        data = response.json()
-        assert "cluster" in data
-        assert "name" in data
-
-    def test_dataframe_functions(self):
-        data_frame = load_dataframe(TEST_DATA_COPY_PATH)
-        assert isinstance(data_frame, pd.DataFrame)
-        assert not data_frame.empty
-
-        cleaned_data_frame = clean_dataframe(data_frame)
-        assert not cleaned_data_frame.empty
-
-        optimal_clusters = determine_optimal_clusters(cleaned_data_frame)
-        assert optimal_clusters > 0
-
-        kmeans_results = perform_clustering(
-            cleaned_data_frame, optimal_clusters)
-
-        labels = [cluster['clusterNr']
-                  for cluster in kmeans_results['cluster'] for _ in cluster['points']]
-        assert len(labels) == len(cleaned_data_frame)
-
-    def test_select_columns_with_test_data(self):
-        data_frame = load_dataframe(TEST_DATA_COPY_PATH)
-        selected_df = select_columns(data_frame, columns=[0, 2])
-        assert list(selected_df.columns) == ["Merkmal 1", "Merkmal 3"]
-        assert selected_df.shape[1] == 2
+            print(response_1.json())

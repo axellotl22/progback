ebe10413ba7739782e873379a35673730028ab41
schroeer <marvin.schroeer@itebo.de>
2023-09-27 18:36:52 +0200

    Jobs in eine Datenbank schreiben

--- app/main.py
+++ app/main.py
@@ -5,8 +5,12 @@ Hauptmodul der App.
 from fastapi import FastAPI
 from app.routers import clustering_router
 from app.routers import job_router
+from app.services.database_service import DBBase, engine
 
 app = FastAPI()
 
+
 app.include_router(clustering_router.router, prefix="/clustering", tags=["clustering"])
 app.include_router(job_router.router, prefix="/jobs", tags=["jobs"])
+
+DBBase.metadata.create_all(bind=engine)

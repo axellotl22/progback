540d94c8512b2c2c45d43a5e04c7511455115a9f
Alexander Richter <a.richter@geocapture.de>
2023-09-28 15:44:36 +0200

    pytest

--- test/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.environ["TEST_MODE"] = "True"
-os.environ["DEV_MODE"] = "True"
-os.environ["DB_HOST"] = ""
-os.environ["DB_PW"] = ""
-os.environ["DB_PORT"] = ""
-os.environ["DB_SCHEMA"] = ""

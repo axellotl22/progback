8204477c838a0fe3fe62bfb64636f4a74bcebb0f
Andrej Gossen <a.gossen@bb-verpackungen.de>
2023-10-07 22:15:01 +0200

    3D K-Means Cluster

--- app/main.py
+++ app/main.py
@@ -6,7 +6,9 @@ from fastapi import FastAPI
 from app.routers import (all_in_one_clustering, 
                          basic_kmeans_router, 
                          elbow_router,
-                         advanced_kmeans_router)
+                         advanced_kmeans_router,
+                         basic_three_d_kmeans_router,
+                         advanced_three_d_kmeans_router)
 
 from app.database import user_db, job_db
 from app.routers import classification_router_decision_tree
@@ -26,9 +28,17 @@ app.include_router(all_in_one_clustering.router, prefix="/all_in_one", tags=["km
 app.include_router(basic_kmeans_router.router, prefix="/basic", tags=["2D kmeans"])
 app.include_router(advanced_kmeans_router.router, prefix="/advanced", tags=["2D kmeans"])
 
-
 # This comment can be removed once the frontend team has taken over the new routers.
 app.include_router(all_in_one_clustering.router, prefix="/clustering", tags=["clustering"])
+
+
+
+# ----------------------- 3D kmeans Routers ---------------------------------------------
+app.include_router(basic_three_d_kmeans_router.router, prefix="/basic", tags=["3D kmeans"])
+app.include_router(advanced_three_d_kmeans_router.router, prefix="/advanced", tags=["3D kmeans"])
+
+
+
 # ----------------------- Elbow Router --------------------------------------------------
 app.include_router(elbow_router.router, prefix="/determination", tags=["determination"])
 
---
+++ app/routers/basic_three_d_kmeans_router.py
@@ -0,0 +1,65 @@
+"""
+three_d_kmeans_router.py
+------------------------
+API router for performing 3D KMeans clustering.
+"""
+
+from fastapi import APIRouter, UploadFile, File, HTTPException, Query
+from app.services.three_d_basic_kmeans_service import perform_3d_kmeans_from_file
+from app.services.custom_kmeans import BaseOptimizedKMeans
+
+router = APIRouter()
+
+@router.post("/perform-3d-kmeans/")
+# pylint: disable=too-many-arguments
+async def kmeans_3d(
+    file: UploadFile = File(...),
+    column_1: int = Query(0, alias="Column 1",
+                          description="Index of the first column"),
+    column_2: int = Query(1, alias="Column 2",
+                          description="Index of the second column"),
+    column_3: int = Query(2, alias="Column 3",
+                          description="Index of the third column"),
+    distance_metric: str = Query(
+            "EUCLIDEAN",
+            description="/".join(BaseOptimizedKMeans.supported_distance_metrics.keys())),
+    kmeans_type: str = Query("OptimizedKMeans",
+                             description="OptimizedKMeans/OptimizedMiniBatchKMeans"),
+    n_clusters: int = Query(2, description="Number of clusters"),
+    user_id: int = Query(0, description="User ID"),
+    request_id: int = Query(0, description="Request ID")
+):
+    """
+    Endpoint for 3D KMeans clustering.
+
+    Args:
+    - file (UploadFile): Uploaded data file.
+    - column_1 (int): Index of the first column.
+    - column_2 (int): Index of the second column.
+    - column_3 (int): Index of the third column.
+    - distance_metric (str): Distance metric for clustering.
+    - kmeans_type (str): Type of KMeans model to use.
+    - n_clusters (int): Number of clusters.
+    - user_id (int): User ID.
+    - request_id (int): Request ID.
+    
+    Returns:
+    - KMeansResult3D: Result of the 3D KMeans clustering.
+    """
+    try:
+        kmeans_result_3d = perform_3d_kmeans_from_file(
+            file=file,
+            user_k=n_clusters,
+            distance_metric=distance_metric,
+            kmeans_type=kmeans_type,
+            user_id=user_id,
+            request_id=request_id,
+            selected_columns=[column_1, column_2, column_3]
+        )
+        # Return the KMeansResult3D object.
+        return kmeans_result_3d
+    # pylint: disable=duplicate-code
+    except ValueError as error:
+        raise HTTPException(400, "Unsupported file type") from error
+    except Exception as error:
+        raise HTTPException(500, "Error processing file") from error
